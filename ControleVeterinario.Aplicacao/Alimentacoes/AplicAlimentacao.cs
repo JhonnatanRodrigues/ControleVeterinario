9fedc8dc12aaad57cc3063809fe350b8152f1e62
JhonnatanRodrigues <76016938+JhonnatanRodrigues@users.noreply.github.com>
2022-11-19 19:25:26 -0300

    correção

--- ControleVeterinario.Aplicacao/Alimentacoes/AplicAlimentacao.cs
+++ ControleVeterinario.Aplicacao/Alimentacoes/AplicAlimentacao.cs
@@ -21,53 +21,72 @@ namespace ControleVeterinario.Aplicacao.Alimentacoes
 
         public void FoiAlimentar(string idRfid)
         {
-            var rfids = _repRFID.Where(x => x.CodigoRFID == idRfid && x.Ativo && x.EmUso).ToList();
-
-            if (rfids == null)
-                throw new Exception($"Não foi encontrado nem um RFID do código '{idRfid}'");
-            if (rfids.Count > 1)
-                throw new Exception($"Foi encontrado mais de um RFID do código '{idRfid}', ativo e em uso.");
-
-            var animais = _repCadastroAnimal.Where(x => x.CodigoRfId == rfids.FirstOrDefault().Id).ToList();
-
-            if (animais.Count > 1)
-                throw new Exception($"Foi encontrado mais de um animal utilizando o RFID '{idRfid}'.");
-
-            _repAlimentacao.Inserir(new Alimentacao
+            try
             {
-                Animal = animais.FirstOrDefault(),
-                CodigoAnimal = animais.FirstOrDefault().Id,
-                DataHora_FoiCome = DateTime.Now,
-                DataHora_ParoCome = null,
-                ParoCome = false
-            });
+                var rfids = _repRFID.Where(x => x.CodigoRFID == idRfid && x.Ativo && x.EmUso).ToList();
+
+                if (rfids == null || rfids.Count == 0)
+                    throw new Exception($"Não foi encontrado nem um RFID do código '{idRfid}'");
+                if (rfids.Count > 1)
+                    throw new Exception($"Foi encontrado mais de um RFID do código '{idRfid}', ativo e em uso.");
+
+                var animais = _repCadastroAnimal.Where(x => x.CodigoRfId == rfids.FirstOrDefault().Id).ToList();
+
+                if (animais == null || animais.Count == 0)
+                    throw new Exception($"Não foi encontrado nem um animal com o RFID '{idRfid}'");
+
+                if (animais.Count > 1)
+                    throw new Exception($"Foi encontrado mais de um animal utilizando o RFID '{idRfid}'.");
+
+                _repAlimentacao.Inserir(new Alimentacao
+                {
+                    Animal = animais.FirstOrDefault(),
+                    CodigoAnimal = animais.FirstOrDefault().Id,
+                    DataHora_FoiCome = DateTime.Now,
+                    DataHora_ParoCome = null,
+                    ParoCome = false
+                });
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+            
 
         }
 
         public void ParoAlimentar(string idRfid)
         {
-            var rfids = _repRFID.Where(x => x.CodigoRFID == idRfid && x.Ativo && x.EmUso).ToList();
+            try
+            {
+                var rfids = _repRFID.Where(x => x.CodigoRFID == idRfid && x.Ativo && x.EmUso).ToList();
 
-            if (rfids == null)
-                throw new Exception($"Não foi encontrado nem um RFID do código '{idRfid}'");
-            if (rfids.Count > 1)
-                throw new Exception($"Foi encontrado mais de um RFID do código '{idRfid}', ativo e em uso.");
+                if (rfids == null)
+                    throw new Exception($"Não foi encontrado nem um RFID do código '{idRfid}'");
+                if (rfids.Count > 1)
+                    throw new Exception($"Foi encontrado mais de um RFID do código '{idRfid}', ativo e em uso.");
 
-            var animais = _repCadastroAnimal.Where(x => x.CodigoRfId == rfids.FirstOrDefault().Id).ToList();
+                var animais = _repCadastroAnimal.Where(x => x.CodigoRfId == rfids.FirstOrDefault().Id).ToList();
 
-            if (animais.Count > 1)
-                throw new Exception($"Foi encontrado mais de um animal utilizando o RFID '{idRfid}'.");
+                if (animais.Count > 1)
+                    throw new Exception($"Foi encontrado mais de um animal utilizando o RFID '{idRfid}'.");
 
-            var alimentacao = _repAlimentacao.Where(x => x.CodigoAnimal == animais.FirstOrDefault().Id && !x.ParoCome).FirstOrDefault();
+                var alimentacao = _repAlimentacao.Where(x => x.CodigoAnimal == animais.FirstOrDefault().Id && !x.ParoCome).FirstOrDefault();
 
-            if (alimentacao == null)
-                throw new Exception($"Não foi encontrado nem um registro do RFID {idRfid}, na fila de alimentação.");
+                if (alimentacao == null)
+                    throw new Exception($"Não foi encontrado nem um registro do RFID {idRfid}, na fila de alimentação.");
 
-            alimentacao.DataHora_ParoCome = DateTime.Now;
-            alimentacao.ParoCome = true;
+                alimentacao.DataHora_ParoCome = DateTime.Now;
+                alimentacao.ParoCome = true;
 
-            _repAlimentacao.SaveChanges();
+                _repAlimentacao.SaveChanges();
+            }
+            catch (Exception ex)
+            {
 
+                throw new Exception(ex.Message);
+            }
         }
 
         public List<Alimentacao> Listar()
